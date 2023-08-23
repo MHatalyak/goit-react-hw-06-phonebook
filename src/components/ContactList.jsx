2f1ab92535c510502e9f45ecd2f848d3c979f836
MHatalyak <matronahu@gmail.com>
2023-08-23 14:19:54 +0300

    Update ContactList.jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -13,9 +13,19 @@ import {
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+
+  const getFilteredContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = getFilteredContacts();
   return (
     <List>
-      {contacts.map(contact => (
+      {filteredContacts.map(contact => (
         <ListItem key={contact.id}>
           <ContactName>{contact.name}:</ContactName>
           <ContactNumber>{contact.number}</ContactNumber>
